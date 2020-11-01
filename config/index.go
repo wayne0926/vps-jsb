c47e75420fa036ebd47a7b6da02b5a20909ea85b
wayne0926 <wayne0926@foxmail.com>
2020-11-01 17:48:10 +0800

    first

---
+++ config/index.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	T "mirror/tool"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Replaced struct {
+	Old string `yaml:"old"`
+	New string `yaml:"new"`
+}
+
+type Yaml struct {
+	Host struct {
+		Self  string `yaml:"self"`
+		Proxy string `yaml:"proxy"`
+	}
+	ReplacedURLs []Replaced `yaml:"replaced_urls"`
+	EnableSSL    bool       `yaml:"enable_ssl"`
+	HandleCookie bool       `yaml:"handle_cookie"`
+}
+
+var data = `
+enable_ssl: True
+handle_cookie: True
+
+host:
+  self: wiki.wr0926.ml
+  proxy: weiji.wr0926.ml
+replaced_urls:
+  - old: weiji.wr0926.ml
+    new: wiki.wr0926.ml
+`
+var Config *Yaml
+var Protocal string
+
+func LoadConfig() {
+	Config = new(Yaml)
+	err := yaml.Unmarshal([]byte(data), Config)
+	T.CheckErr(err)
+	if Config.EnableSSL {
+		Protocal = "https://"
+	} else {
+		Protocal = "http://"
+	}
+}
---
+++ docs/README.md
@@ -0,0 +1,1 @@
+# 把Vercel当我的CDN/xyx
